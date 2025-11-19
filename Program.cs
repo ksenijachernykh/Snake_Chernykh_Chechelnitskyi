f5d13b828d88a2c6cb26da588cc2439b2e9f6578
Your Name <you@example.com>
2025-11-19 16:15:28 +0500

    Добавление нового проекта.Черных

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Snake_Chernykh_Chechelnitskyi
 {
-    internal class Program
+    class Program
     {
+        public static List<Leadres> Leadres = new List<Leadres>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        private static int localPort = 5001;
+        public static int MaxSpeed = 15;
         static void Main(string[] args)
         {
         }
