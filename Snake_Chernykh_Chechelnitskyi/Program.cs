8f318f20a5d3ae09b30321912d021da9af96ec5a
Your Name <you@example.com>
2025-11-19 16:46:31 +0500

    Создание метода Send.Черных

--- Snake_Chernykh_Chechelnitskyi/Program.cs
+++ Snake_Chernykh_Chechelnitskyi/Program.cs
@@ -1,20 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
+using Newtonsoft.Json;
 
 namespace Snake_Chernykh_Chechelnitskyi
 {
     class Program
     {
-        public static List<Leadres> Leadres = new List<Leadres>();
+        public static List<Leaders> Leadres = new List<Leaders>();
         public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
         public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         private static int localPort = 5001;
         public static int MaxSpeed = 15;
         static void Main(string[] args)
         {
+        }
+
+        private static void Send()
+        {
+            foreach(ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Возникло исключение: " + ex.ToString() + "\n " + ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
         }
     }
 }
