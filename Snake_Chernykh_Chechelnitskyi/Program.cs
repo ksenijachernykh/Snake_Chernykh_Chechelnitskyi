a27a8c7e0e89416ce03b6f6b36665698e3bde804
Kseniya <ksenijachernyh@yandex.ru>
2025-11-21 22:28:49 +0300

    Проверка

--- Snake_Chernykh_Chechelnitskyi/Program.cs
+++ Snake_Chernykh_Chechelnitskyi/Program.cs
@@ -20,6 +20,7 @@ namespace Snake
         public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         private static int localPort = 5001;
         public static int MaxSpeed = 15;
+
         private static void Send()
         {
             foreach (ViewModelUserSettings User in remoteIPAddress)
